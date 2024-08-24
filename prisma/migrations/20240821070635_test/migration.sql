638de3283152d98520ed7461c09a14f66df3cec1
dev-simpleplan <95477430+dev-simpleplan@users.noreply.github.com>
2024-08-24 16:27:12 +0530

    Delete prisma/migrations/20240821070635_test directory

--- prisma/migrations/20240821070635_test/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Analytics" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "revenue" TEXT NOT NULL,
-    "orders" TEXT NOT NULL,
-    "currency" TEXT,
-    "userId" TEXT NOT NULL,
-    CONSTRAINT "Analytics_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Session" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);

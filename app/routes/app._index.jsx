4d5b93245605a66886679d895ea5835b4f5f0d42
dev-simpleplan <95477430+dev-simpleplan@users.noreply.github.com>
2024-09-16 18:07:27 +0530

    Update app._index.jsx

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useMemo } from "react";
-import { useLoaderData, useFetcher } from "@remix-run/react";
+import { useLoaderData, useFetcher, useNavigate, useNavigation } from "@remix-run/react";
 import { json, redirect } from "@remix-run/node";
 import { useAppBridge } from "@shopify/app-bridge-react";
-import { fetchBundle } from "../fetch-bundle.server";
+// import { fetchBundle } from "../fetch-bundle.server";
 import { DashboardUI } from "../components/Dashboard";
 import Onboarding from "../components/Onboard";
 import { authenticate } from "../shopify.server";
@@ -11,17 +11,28 @@ import { UPDATE_PRODUCT_MUTATION } from "../api/UPDATE_PRODUCT_MUTATION";
 import { DELETE_PRODUCT_MUTATION } from "../api/DELETE_PRODUCT_MUTATION";
 import { handleGraphQLResponse } from "../utils/sharedUtils";
 import { submitToGoogleSheets } from "../server/google-spreadsheet.server";
+import { Spinner } from "@shopify/polaris";
+import { REGISTER } from "../api/REGISTER";
 
 export async function loader({ request }) {
-  const { session } = await authenticate.admin(request);
+  const { session, admin } = await authenticate.admin(request);
 
   const sessionData = await prisma.session.findUnique({
-    where: { id: session.id }
+    where: { id: session.id },
+  });
+  const response = await admin.graphql(REGISTER);
+  const jsonResponse = await response.json(); // Await the JSON response
+  console.log(jsonResponse); // Log the actual JSON response
+
+  const bundleFromDb = await prisma.bundle.findMany({
+    where: {
+      userId: session.id,
+    },
   });
 
-  const data = await fetchBundle(request);
-  data.session = sessionData;
-  return json(data);
+  // const data = await fetchBundle(request);
+  const data = bundleFromDb;
+  return json({ products: data, session: sessionData });
 }
 
 async function updateProductStatus(admin, productId, newStatus) {
@@ -29,21 +40,26 @@ async function updateProductStatus(admin, productId, newStatus) {
     variables: {
       input: {
         id: productId,
-        status: newStatus.toUpperCase()
-      }
-    }
+        status: newStatus.toUpperCase(),
+      },
+    },
   });
 
   await handleGraphQLResponse(response, "Error updating product details");
 }
 
-async function deleteProduct(admin, productId) {
+async function deleteProduct(admin, productId, productIdInDB) {
+  const bundleID = parseInt(productIdInDB);
   const response = await admin.graphql(DELETE_PRODUCT_MUTATION, {
     variables: {
       input: {
-        id: productId
-      }
-    }
+        id: productId,
+      },
+    },
+  });
+
+  const deleteBundle = await prisma.bundle.delete({
+    where: { id: bundleID },
   });
 
   await handleGraphQLResponse(response, "Error deleting product");
@@ -63,21 +79,35 @@ export async function action({ request }) {
       const shopDomain = session.shop;
 
       if (!email || !message || !requestType) {
-        return json({ success: false, error: "Missing required fields" }, { status: 400 });
+        return json(
+          { success: false, error: "Missing required fields" },
+          { status: 400 }
+        );
       }
 
-      const result = await submitToGoogleSheets({ email, message, requestType, timestamp, shopDomain });
+      const result = await submitToGoogleSheets({
+        email,
+        message,
+        requestType,
+        timestamp,
+        shopDomain,
+      });
 
       if (result.success) {
-        return json({ success: true, message: "Support request submitted successfully" });
+        return json({
+          success: true,
+          message: "Support request submitted successfully",
+        });
       } else {
         return json({ success: false, error: result.error }, { status: 500 });
       }
     } catch (error) {
-      return json({ success: false, error: "Failed to submit support request" }, { status: 500 });
+      return json(
+        { success: false, error: "Failed to submit support request" },
+        { status: 500 }
+      );
     }
   }
-
 
   if (action === "updateOnboarding") {
     try {
@@ -87,7 +117,10 @@ export async function action({ request }) {
       });
       return redirect("/app/create-bundle");
     } catch (error) {
-      return json({ success: false, error: "Failed to update onboarding status" }, { status: 500 });
+      return json(
+        { success: false, error: "Failed to update onboarding status" },
+        { status: 500 }
+      );
     }
   }
 
@@ -96,20 +129,28 @@ export async function action({ request }) {
     const newStatus = formData.get("newStatus");
 
     try {
-      await updateProductStatus(admin, productId, newStatus);      
+      await updateProductStatus(admin, productId, newStatus);
       return json({ success: true });
     } catch (error) {
-      return json({ success: false, error: "Failed to update product status" }, { status: 500 });
+      return json(
+        { success: false, error: "Failed to update product status" },
+        { status: 500 }
+      );
     }
   }
 
   if (action === "deleteProduct") {
     const productId = formData.get("productId");
+    const productIdInDB = formData.get("productIdInDB");
+
     try {
-      await deleteProduct(admin, productId);
+      await deleteProduct(admin, productId, productIdInDB);
       return json({ success: true });
     } catch (error) {
-      return json({ success: false, error: "Failed to delete product" }, { status: 500 });
+      return json(
+        { success: false, error: "Failed to delete product" },
+        { status: 500 }
+      );
     }
   }
 
@@ -120,9 +161,11 @@ export default function Dashboard() {
   const data = useLoaderData();
   const fetcher = useFetcher();
   const app = useAppBridge();
+  const navigate = useNavigate();
+  const navigation = useNavigation();
 
   const products = useMemo(() => {
-    return data?.data?.products?.edges || [];
+    return data.products || [];
   }, [data]);
 
   const handleStatusChange = (productId, newStatus) => {
@@ -132,9 +175,9 @@ export default function Dashboard() {
     );
   };
 
-  const handleDeleteProduct = (productId) => {
+  const handleDeleteProduct = (productId, productIdInDB) => {
     fetcher.submit(
-      { action: "deleteProduct", productId },
+      { action: "deleteProduct", productId, productIdInDB },
       { method: "post" }
     );
   };
@@ -142,22 +185,32 @@ export default function Dashboard() {
   useEffect(() => {
     if (fetcher.data) {
       if (fetcher.data.success) {
-        app.toast.show('Operation completed successfully');
+        app.toast.show("Operation completed successfully");
       } else if (fetcher.data.error) {
         app.toast.show(fetcher.data.error, { isError: true });
       }
     }
   }, [fetcher.data, app]);
-
   
 
+  if (navigation.state === "loading") {
+    return (
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <Spinner accessibilityLabel="Loading" size="large" />
+      </div>
+    );
+  }
+
   if (data.session?.onboarding) {
-    return <DashboardUI 
-      products={products} 
-      onStatusChange={handleStatusChange} 
-      onDeleteProduct={handleDeleteProduct}
-      fetcher={fetcher}
-    />;
+    return (
+      <DashboardUI
+        products={products}
+        onStatusChange={handleStatusChange}
+        onDeleteProduct={handleDeleteProduct}
+        fetcher={fetcher}
+        navigate={navigate}
+      />
+    );
   }
 
   return <Onboarding />;
